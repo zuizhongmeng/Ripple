8f911a0860f0908cffc3fff4d85d4ab21061e618
zuizhongmeng <youkoutou@gmail.com>
2025-10-24 13:56:06 +0900

    【标题】更新 【内容】 【其他】

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # -------------------------------
 
 # Ollama 本地服务地址
-DEFAULT_OLLAMA_URL = "http://localhost:11434/api/generate"
+DEFAULT_OLLAMA_URL = "https://12vqlm-ip-49-98-40-252.tunnelmole.net/api/generate"
 
 # OpenAI 中继服务地址（如使用本地代理）
 DEFAULT_OPENAI_RELAY_URL = "http://localhost:8000/ask"
